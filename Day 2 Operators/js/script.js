1912c12540b4dcd57bd2a923677a736ea899fbf2
Aamir Shaikh <aamir729039@gmail.com>
2025-06-30 10:55:08 +0530

    firstcommit

---
+++ Day 2 Operators/js/script.js
@@ -0,0 +1,36 @@
+//Arithmetic Operators
+
+let a=10;
+let b=5;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+
+//Assingment Operators
+
+console.log(a += 10);
+console.log(a -= 10);
+console.log(a /= 10);
+console.log(a *= 10);
+console.log(a %= 10);
+
+//Comparison Operators
+
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a <= b);
+console.log(a === b);
+console.log(a !== b);
+
+//Logical Operators
+
+console.log(a >= 5 && b <= 6);
+console.log(a >= 5 && b <= 4);
+console.log(a >= 5 || b <= 6);
+console.log(a >= 5 || b <= 1);
+console.log(!(a >= 6));
+
---
+++ Day 2 Operators/js/script2.js
@@ -0,0 +1,13 @@
+//
+
+let avg=parseInt(prompt("Enter your avg: "));
+
+avg>=35
+    ?avg>=80
+        ?console.log("A Grade")
+         :avg>=60
+            ?console.log("B Grade")
+                :avg>=50
+                    ?console.log("C Grade")
+                    :console.log("Pass Class")
+                :alert("Wrong Input")
