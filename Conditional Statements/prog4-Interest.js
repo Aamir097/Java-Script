dac54a752b5338f6cb521d0cce68f62180923a6e
Aamir Shaikh <aamir729039@gmail.com>
2025-07-08 09:04:23 +0530

    Initial Commit

--- Conditional Statements/prog4-Interest.js
+++ Conditional Statements/prog4-Interest.js
@@ -1,12 +1,12 @@
 // Calculate Interest
 
-// if amount 5L is invested for 5 years the intrest 7 % per annum.
+// if amount 5L is invested for 5 years the interest 7 % per annum.
 // else if amount 3L is invested for 3 years the intrest 5 % per annum.
 // else if amount 2L is invested for 3 years the intrest 4.2 % per annum.
 // else  2.5 % per annum.
 
-let amount = 800000; 
-let years = 2;
+let amount = 500000; 
+let years = 5;
 
 if(amount === 500000 && years === 5){
     console.log('Rate is: 7%');
